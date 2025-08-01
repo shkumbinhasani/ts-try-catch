c0cebc59342eec9d382cfda391f30a58f3f2fd74
shkumbinhasani <sh131171@gmail.com>
2025-08-01 15:20:16 +0200

    Bump version to 0.0.2 and simplify DataError type definition

--- lib/try-catch.d.ts
+++ lib/try-catch.d.ts
@@ -7,20 +7,7 @@ export type Throws<T extends Error> = {
 
 type ExtractErrors<T> = T extends Throws<infer E> ? E : never;
 
-type Simplify<T> = T extends any ? { [K in keyof T]: T[K] } : never;
-
-type ExtractData<T> =
-    T extends string ? string :
-        T extends number ? number :
-            T extends boolean ? boolean :
-                T extends symbol ? symbol :
-                    T extends bigint ? bigint :
-                        T extends null ? null :
-                            T extends undefined ? undefined :
-                                T extends Function ? T :
-                                    Simplify<Omit<T, ErrorSymbol>>;
-
-type DataError<T> = [ExtractData<T>, null] | [null, Error | ExtractErrors<T>];
+type DataError<T> = [T, null] | [null, Error | ExtractErrors<T>];
 type TryCatchReturn<T> = T extends Promise<infer R> ? Promise<DataError<R>> : DataError<T>;
 
 export declare function tryCatch<T>(fn: () => T): TryCatchReturn<T>;
